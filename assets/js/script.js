31d94f48c6a532692fd9fd7e724c61e404c701d7
TiagoCarvalho-true <carvalhost58@gmail.com>
2024-10-29 11:13:53 -0400

    Consertando a rsponsividade, e outras funcoes, primeira funçao js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function toggleText(){
+    let content = document.getElementById("content");
+    if(content === none){
+        content.style.display ="block";
+
+    } else{
+        content.style.display = "none";
+    }
+}
